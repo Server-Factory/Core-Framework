80982f209099752b5df068145fac167a2e4ff42e
Milos Vasic <milos85vasic@gmail.com>
2021-05-22 20:07:14 +0200

    WSF-4 - Small cleanup.

--- Framework/src/main/kotlin/net/milosvasic/factory/deployment/source/TargetSourceValidator.kt
+++ Framework/src/main/kotlin/net/milosvasic/factory/deployment/source/TargetSourceValidator.kt
@@ -2,16 +2,14 @@ package net.milosvasic.factory.deployment.source
 
 import net.milosvasic.factory.common.validation.ValidationAsync
 import net.milosvasic.factory.common.validation.ValidationCallback
-import net.milosvasic.factory.execution.TaskExecutor
 import net.milosvasic.factory.execution.flow.callback.FlowCallback
 import net.milosvasic.factory.execution.flow.implementation.CommandFlow
+import net.milosvasic.factory.log
 import net.milosvasic.factory.terminal.Terminal
 import net.milosvasic.factory.terminal.command.ApplicationInfoCommand
 import net.milosvasic.factory.terminal.command.GitVerifyRepository
 
 class TargetSourceValidator : ValidationAsync<TargetSource> {
-
-    private val executor = TaskExecutor.instantiate(1)
 
     @Throws(IllegalArgumentException::class, IllegalStateException::class)
     override fun validate(what: TargetSource, callback: ValidationCallback) {
@@ -23,6 +21,10 @@ class TargetSourceValidator : ValidationAsync<TargetSource> {
 
                     override fun onFinish(success: Boolean) {
 
+                        if (!success) {
+
+                            log.e("Invalid target source: ${what.value}")
+                        }
                         callback.onValidated(success)
                     }
                 }
